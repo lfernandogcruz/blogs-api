91429ece8556b7eb67343152b651d78c5a5c1283
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-08-25 20:47:24 -0300

    feat: req 02 - creates User model with the given properties

---
+++ src/database/models/user.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  });
+
+  return User;
+};
