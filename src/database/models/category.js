812ac2b2d41a8647dfd1fc910db4411f7fa6d2e9
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-08-25 23:34:06 -0300

    feat: req 07 - creates Category model with the given properties

---
+++ src/database/models/category.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define("Category", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    name: DataTypes.STRING,
+  });
+
+  return Category;
+};
