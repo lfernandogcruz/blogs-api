5f513c51b2e1d47387e898e019cecae27b2fadc5
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-08-26 20:22:06 -0300

    feat: req 04 - creates Controller for POST Create a new User with proper validations and token

--- src/database/controller/index.js
+++ src/database/controller/index.js
@@ -1,4 +1,6 @@
 const jwt = require('jsonwebtoken');
+const helpers = require('../helpers');
+const services = require('../services');
 
 const { JWT_SECRET } = process.env;
 
@@ -7,6 +9,15 @@ const controllers = {
     const { email } = req.body;
     const token = jwt.sign({ email }, JWT_SECRET, { expiresIn: '1d' });
     return res.status(200).json({ token });
+  },
+  createUser: async (req, res) => {
+    const { displayName, email,
+      password, image } = req.body;
+    const obj = { displayName, email, password, image }; 
+    const isCreated = await services.create(obj);
+    if (!isCreated) return res.status(500).json({ message: 'Internal Error' });
+    const token = helpers.tokenGen({ email, password });
+    return res.status(201).json({ token });
   },
 };
 
