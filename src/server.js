bc0dfddd9fc2de0734562803ecd93f48cf2c9c02
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-08-26 14:12:08 -0300

    feat: req 03 - creates GET route for Login, updates Middlewares for POST on Login and change Error log on Error Middleware

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
 const app = require('./api');
+const model = require('./database/models');
+const middleware = require('./database/middlewares');
+const controller = require('./database/controller');
 
 // não remova a variável `API_PORT` ou o `listen`
 const port = process.env.API_PORT || 3000;
@@ -7,6 +10,25 @@ const port = process.env.API_PORT || 3000;
 // não remova esse endpoint
 app.get('/', (_request, response) => {
   response.send();
+});
+
+app.get('/login', model.User);
+app.post('/login',
+  middleware.loginEmptyFieldsValidation,
+  middleware.loginValidation,
+  controller.userLogin);
+
+app.use((err, _req, res, _next) => {
+  if (err.message === 'connect ECONNREFUSED 127.0.0.1:3306') {
+    return res.status(500).json({ message: 'DB OFFLINE' });
+  }
+  if (err.message === 'connect ECONNREFUSED 127.0.0.1:3300') {
+    return res.status(500).json({ message: 'DB OFFLINE' });
+  }
+  if (err.message === 'connect ECONNREFUSED 127.0.0.1:3030') {
+    return res.status(500).json({ message: 'DB OFFLINE' });
+  }
+  return res.status(500).json({ message: 'Unknown ERROR' });
 });
 
 app.listen(port, () => console.log('ouvindo porta', port));
